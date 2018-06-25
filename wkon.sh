d3ddac961455670d2f8ff2a1394b108dcd100212
gillchristian <gillchristiang@gmail.com>
2018-06-25 22:55:39 +0200

    Remove message on outpu and update help message

--- wkon.sh
+++ wkon.sh
@@ -2,9 +2,6 @@
 #         What I'm Working ON          #
 ########################################
 
-# zsh on directoy change hook
-add-zsh-hook chpwd wkon;
-
 function wkon () {
   file="${WKON_FILE:-WORKING-ON.txt}";
 
@@ -18,14 +15,15 @@ function wkon () {
 
   # -h, --help flag
   if [ "$1" == "-h" ] || [ "$1" == "--help" ] ; then
-    printf "wkon: easy keep track of what you are working on\n\n";
+    printf "wkon: easily keep track of what you are working on\n\n";
     printf "USAGE:\n";
-    printf "  $ wkon [message] # appends [message] to %s and displays it's content\n\n" "$file";
+    printf "  $ wkon [message] # appends [message] to \$WKON_FILE and displays it's content\n";
+    printf "  $ wkon           # display \$WKON_FILE content\n\n";
     printf "CONFIGURATION:\n";
-    printf "  Use \$WKON_FILE to save to a custom file. Defatul: \"WORKING-ON.txt\"\n\n";
+    printf "  \$WKON_FILE defaults to \"WORKING-ON.txt\", change it to use a different file.\n\n";
     printf "OPTIONS:\n";
     printf "  -h, --help    show help\n";
-    printf "  -d, --delete  remove %s\n" "$file";
+    printf "  -d, --delete  removes \$WKON_FILE\n";
 
     return;
   fi
@@ -38,7 +36,6 @@ function wkon () {
 
   # only show content if file exists and has content
   if [ -f $file ] && [ -s $file ]; then
-    echo "----- WORKING ON: -----\n";
     cat $file;
   fi
 }
